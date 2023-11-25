d82f7a6043d1f3ce3bf936c58731c88b3c1b4d61
Francisco Juan <juansoussev@gmail.com>
2023-11-25 17:28:05 -0300

    criando um controller para os users

---
+++ src/user.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post, Body, Get } from "@nestjs/common";
+import { UserRepository } from './user.repository';
+
+@Controller("/users")
+export class UserController {
+
+    private userRepository = new UserRepository()
+
+    @Post()
+    async createUser(@Body() userData) {
+        this.userRepository.save(userData)
+        return userData
+    }
+
+    @Get()
+    async listUsers() {
+        return this.userRepository.list()
+    }
+}
---
+++ src/user.repository.ts
@@ -0,0 +1,13 @@
+
+
+export class UserRepository {
+    private users = [];
+
+    async save(user) {
+        this.users.push(user)
+    }
+
+    async list() {
+        return this.users
+    }
+}
