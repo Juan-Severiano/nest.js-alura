62cdff5699d3946eccd89e9136a7d842fcb7fe19
Francisco Juan <juansoussev@gmail.com>
2023-12-25 20:49:42 -0300

    db data hidden

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import UserModule from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostgresConfigService } from './config/postgres.config.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, TypeOrmModule.forRootAsync({
-    useClass: PostgresConfigService,
-    inject: [PostgresConfigService]
-  })]
+  imports: [
+    UserModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ]
 })
-export class AppModule {}
+export class AppModule { }
